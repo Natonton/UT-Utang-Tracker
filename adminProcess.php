a6345221236b13ba4d6d8e817fb2da681472a92b
lloyd <lloydgabrielnatonton@gmail.com>
2024-01-23 10:17:05 +0800

    sms notif

--- adminProcess.php
+++ adminProcess.php
@@ -331,4 +331,59 @@
                 <?php
         }
     }
+    //notify thru sms
+    if(isset($_POST['notify'])){
+        $number = $_POST['number'];
+        $messagee = $_POST['message'];
+
+        $service_plan_id = "5103d9ee24154b8db545db030b847884";
+        $bearer_token = "bc552468af7f4031b86eb8bce2df45b6";
+
+        //Any phone number assigned to your API
+        $send_from = "+447520651367";
+        //May be several, separate with a comma ,
+        $recipient_phone_numbers = $number; 
+        // $message = "Test message to {$recipient_phone_numbers} from {$send_from}";
+
+        // Check recipient_phone_numbers for multiple numbers and make it an array.
+        if(stristr($recipient_phone_numbers, ',')){
+        $recipient_phone_numbers = explode(',', $recipient_phone_numbers);
+        }else{
+        $recipient_phone_numbers = [$recipient_phone_numbers];
+        }
+
+        // Set necessary fields to be JSON encoded
+        $content = [
+        'to' => array_values($recipient_phone_numbers),
+        'from' => $send_from,
+        'body' => $messagee
+        ];
+
+        $data = json_encode($content);
+
+        $ch = curl_init("https://us.sms.api.sinch.com/xms/v1/{$service_plan_id}/batches");
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+        curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BEARER);
+        curl_setopt($ch, CURLOPT_XOAUTH2_BEARER, $bearer_token);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+        $result = curl_exec($ch);
+
+        if(curl_errno($ch)) {
+            echo 'Curl error: ' . curl_error($ch);
+        } else {
+            // echo $result;
+            ?>
+                <script>
+                    alert("Notification successfully sent");
+                    window.location.href="debtors.php";
+                </script>
+            <?php
+        }
+        curl_close($ch);
+        
+    }
 ?>
